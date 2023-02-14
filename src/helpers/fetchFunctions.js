ff80fbef44d94a2e6acac3bb5bb7b59b006812a4
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-14 16:51:24 -0300

    Resolve requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,18 @@
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async ($QUERY) => {
+  if (!$QUERY) {
+    throw new Error('Termo de busca não informado');
+  }
+
+  const joinURL = `${API_URL}${$QUERY}`;
+
+  const response = await fetch(joinURL);
+  const data = await response.json();
+
+  return data.results;
 };
