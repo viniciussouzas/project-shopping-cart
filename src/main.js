64a57f4853de8a9d1313fbd5a740c6c2f8c9fa96
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-15 12:30:15 -0300

    Resolve requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const productsList = document.querySelector('.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const listProducts = async ($QUERY) => {
+  const result = await fetchProductsList($QUERY);
+
+  result.forEach((product) => productsList.appendChild(createProductElement(product)));
+};
+
+listProducts('computador');
